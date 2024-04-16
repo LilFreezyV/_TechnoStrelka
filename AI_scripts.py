a280e8fd3c154a41840296b4dddae8a67f5686fe
anvov <an.vova.08@mail.ru>
2024-04-16 12:33:38 +0300

    first commit

---
+++ AI_scripts.py
@@ -0,0 +1,40 @@
+import cv2
+import easyocr
+import numpy
+import imutils
+
+
+def get_car_numbers(file, extension):
+    with open(f'file{extension}', 'wb') as f:
+        f.write(file)
+        img = cv2.imread(f'file{extension}')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # создаём фильтр для фото, чтобы уменьшить шум
+    img_filter = cv2.bilateralFilter(gray, 11, 15, 15)
+    # находим углы изображения
+    edges = cv2.Canny(img_filter, 30, 200)
+    # находим контуры изображения
+    cont = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cont = imutils.grab_contours(cont)
+    cont = sorted(cont, key=cv2.contourArea, reverse=True)
+    position = None
+    for el in cont:
+        approx = cv2.approxPolyDP(el, 12, True)
+        if len(approx) == 4:
+            position = approx
+            break
+
+    mask = numpy.zeros(gray.shape, numpy.uint8)
+    new_img = cv2.drawContours(mask, [position], 0, 255, -1)
+    bitwise_img = cv2.bitwise_and(img, img, mask=mask)
+
+    x, y = numpy.where(mask == 255)
+    x1, y1 = numpy.min(x), numpy.min(y)
+    x2, y2 = numpy.max(x), numpy.max(y)
+    crop = gray[x1:x2, y1:y2]
+
+    text = easyocr.Reader(['en'])
+    text = text.readtext(crop)
+    result = text[0][-2][:6]
+    return result
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import cv2
 import easyocr
 from matplotlib import pyplot as pl
+from AI_scripts import get_car_numbers
 
 DATA = []
 
@@ -46,42 +47,11 @@ def handle_document(message):
     downloaded_file = bot.download_file(file_info.file_path)
     file_extension = os.path.splitext(message.document.file_name)[1]
     bot.send_message(message.chat.id, 'фото в обработке...')
-    with open(f'file{file_extension}', 'wb') as f:
-        f.write(downloaded_file)
-        img = cv2.imread(f'file{file_extension}')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # создаём фильтр для фото, чтобы уменьшить шум
-    img_filter = cv2.bilateralFilter(gray, 11, 15, 15)
-    # находим углы изображения
-    edges = cv2.Canny(img_filter, 30, 200)
-    # находим контуры изображения
-    cont = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cont = imutils.grab_contours(cont)
-    cont = sorted(cont, key=cv2.contourArea, reverse=True)
-    position = None
-    for el in cont:
-        approx = cv2.approxPolyDP(el, 12, True)
-        if len(approx) == 4:
-            position = approx
-            break
+    result = get_car_numbers(downloaded_file, file_extension)
 
-    mask = numpy.zeros(gray.shape, numpy.uint8)
-    new_img = cv2.drawContours(mask, [position], 0, 255, -1)
-    bitwise_img = cv2.bitwise_and(img, img, mask=mask)
-
-    x, y = numpy.where(mask == 255)
-    x1, y1 = numpy.min(x), numpy.min(y)
-    x2, y2 = numpy.max(x), numpy.max(y)
-    crop = gray[x1:x2, y1:y2]
-
-    text = easyocr.Reader(['en'])
-    text = text.readtext(crop)
-    result = text[0][-2][:6]
-
-
-    #pl.imshow(cv2.cvtColor(crop, cv2.COLOR_BGR2RGB))
-    #pl.show()
+    # pl.imshow(cv2.cvtColor(crop, cv2.COLOR_BGR2RGB))
+    # pl.show()
 
     bot.send_message(message.chat.id, result)
 
